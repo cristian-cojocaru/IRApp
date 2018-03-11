bf97e4793cafe66d06a3fa302bd42fa3953af392
Cristian Cojocaru <cristian.cojocaru@live.com>
2018-03-11 21:51:19 +0200

    first commit

---
+++ src/main/java/TextFileIndexer.java
@@ -0,0 +1,26 @@
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+
+import java.io.IOException;
+
+class TextFileIndexer{
+    void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+        Document doc = new Document();
+        doc.add(new TextField("title", title, Field.Store.YES));
+        doc.add(new StringField("isbn", isbn, Field.Store.YES));
+        w.addDocument(doc);
+    }
+
+    StandardAnalyzer analyzer = new StandardAnalyzer();
+    Directory index = new RAMDirectory();
+
+    IndexWriterConfig config = new IndexWriterConfig(analyzer);
+
+}
